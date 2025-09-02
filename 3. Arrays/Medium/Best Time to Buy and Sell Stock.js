4b30be0224455ad3483776d01240ac8346b54bc3
urvashi <urvashi.rawat@celebaltech.com>
2025-09-02 12:51:52 +0530

    updated arrays

--- 3. Arrays/Medium/Best Time to Buy and Sell Stock.js
+++ 3. Arrays/Medium/Best Time to Buy and Sell Stock.js
@@ -1,7 +1,7 @@
 //You are given an array where array[i] is the price of a stock on day i.
 //Find the maximum profit you can achieve by choosing one buy day and one sell day (buy before sell).
 
-function sample(arr) {
+function sample(prices) {
   let minPrice = Infinity;
   let maxProfit = 0;
 
